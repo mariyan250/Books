60b6ed527491d6c9973b58a53485bc0511ca0bbc
Mariyan Velichkov <Mariyan.Velichkov@indeavr.com>
2021-01-30 17:11:13 +0200

    Check if book already exists when try to add new book.

--- routes/books.js
+++ routes/books.js
@@ -1,16 +1,16 @@
 import express from 'express';
-
 import books from '../database.js';
 
 const router = express.Router();
 
+// Get all books
 router.get('/', (req, res) => {
   res.json(books);
 });
 
+// Get book by ID
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-
   const book = books.find((book) => book.id === id);
 
   if (!book) {
@@ -19,6 +19,28 @@ router.get('/:id', (req, res) => {
   }
 
   res.json(book);
+});
+
+// Create a book
+router.post('/', (req, res) => {
+  const { id, name, author, price } = req.body;
+
+  const book = { id, name, author, price };
+  const exists = books.find((book) => book.id == id);
+
+  if (!id && !name && !author && !price) {
+    res.status(404).send('Please enter all required data to publish book!');
+    return;
+  } else if (exists) {
+    res
+      .status(405)
+      .send(
+        'There is already book with that id! Please enter other id for the new book!'
+      );
+    return;
+  }
+
+  books.push(book);
 });
 
 export default router;
